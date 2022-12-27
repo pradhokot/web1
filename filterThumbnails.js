3980426f39acd0df17bc613e77e8cd278ec5619d
pradhokot <99374274+pradhokot@users.noreply.github.com>
2022-12-27 17:20:11 +0700

    fixed input type number

--- filterThumbnails.js
+++ filterThumbnails.js
@@ -1,3 +1,4 @@
+document.write(`
 <nav id="filterThumbnails">
    <button class="btn btn-sm btn-light mb-1">collection : single origin <i class="bi bi-x-circle ml-1"></i></button>
    <button class="btn btn-sm btn-light mb-1">type : arabica <i class="bi bi-x-circle ml-1"></i></button>
@@ -6,3 +7,4 @@
    <button class="btn btn-sm btn-light mb-1">weight : 200g <i class="bi bi-x-circle ml-1"></i></button>
    <button class="btn btn-sm btn-light mb-1">sortby : latest product <i class="bi bi-x-circle ml-1"></i></button>
 </nav>
+`);
